75e86904aff2faf7ee9baa75656d8e3cd1f558f6
Johan Eliasson <johan@eliasson.us>
2019-06-06 00:51:35 +0200

    Updating match of yaml

--- app/src/cli/commands/run.js
+++ app/src/cli/commands/run.js
@@ -13,13 +13,28 @@ program.on('command:run', function (dir) {
 
     console.log('Finding yaml files in config directory...')
 
-    const yamlPattern = /.*\/dockr\/config\/([a-zA-Z0-9]{1,})\/[a-zA-Z0-9]{1,}\.yaml$/
-    const yamlFiles = shell.find(path.config).filter(function (file) { 
-        return file.match(yamlPattern)
+    const yamlPattern = /.*\/dockr\/config\/([a-zA-Z0-9\-\_]{1,})\/([a-zA-Z0-9\-\_]{1,})\.yaml$/
+    const yamlFiles = []
+
+    shell.find(path.config).filter(function (file) {
+        let match = file.match(yamlPattern)
+
+        if (match !== null) {
+            
+            const path = match[0]
+            const app = match[1]
+            const file = match[2]
+
+            yamlFiles.push({
+                path,
+                app,
+                file,
+            })
+        }
     })
 
     yamlFiles.forEach(function(yaml) {
-        consola.log('Processing ' + yaml)
+        consola.log('Processing ' + yaml.app + ' and it\'s file ' + yaml.file + '.yaml')
     })
 
     process.exit(1)
