d20c502e2ffbe9ed71b0db438f3349f02228e0f2
Johan Eliasson <johan@eliasson.us>
2019-07-05 20:42:15 +0200

    Adding starting point of backup command

--- app/src/cli/commands/backup.js
+++ app/src/cli/commands/backup.js
@@ -1,7 +1,10 @@
 const consola = require('consola')
 const colors = require('colors')
 
+const fs = require('fs')
 const program = require('commander')
+
+const path = require('../../utils/path')
 
 program.command('backup', 'make a backup of dockr')
 
@@ -9,6 +12,15 @@ program.on('command:backup', function (dir) {
     
     consola.warn('This command is not yet implemented')
     consola.info('Command should take backup of dockr')
+
+    consola.log()
+    consola.info('Creating backup directory')
+
+    const backupPath = `${path.backup}`
+    if (!fs.existsSync(backupPath)) {
+        fs.mkdirSync(backupPath)
+    }
+    
 
     process.exit(1)
 })
