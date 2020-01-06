2c9f302913260fab372c4126610b47cfa25dc960
Johan Eliasson <jeliasson@users.noreply.github.com>
2020-01-06 02:25:25 +0100

    Typo

--- app/scripts/setup-v2.sh
+++ app/scripts/setup-v2.sh
@@ -49,7 +49,7 @@ sudo ln --symbolic --force ${BIN_PATH}/dockr-v2.sh /usr/local/bin/dr-v2
 # Set owner and permissions
 sudo chown -R $(whoami):$(whoami) ${APP_PATH} 2>&1 >/dev/null
 sudo chmod +x ${BIN_PATH}/* 2>&1 >/dev/null
-sudo chmod +x ${SCRIPT_PATH}/* 2>&1 >/dev/null
+sudo chmod +x ${SCRIPTS_PATH}/* 2>&1 >/dev/null
 
 # Finally, print the banner
 echo
